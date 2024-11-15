39faf4b19a866743f3f2716a3c1d58ad84748b21
iraniangunner <fateh19950@hotmail.com>
2024-11-15 19:54:07 +0330

    tailwind config and fonts config added to project

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,51 @@
 import "./globals.css";
+import { Figtree } from "next/font/google";
+import localFont from "next/font/local";
+import { Header } from "./_components/header/header";
+import { Footer } from "./_components/footer/footer";
+
+const figtree = Figtree({
+  display: "swap",
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-figtree",
+});
+
+const yekanbakh = localFont({
+  src: [
+    {
+      path: "../../public/fonts/yekanbakh/YekanBakhFaNum-Thin.woff2",
+      weight: "100",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/yekanbakh/YekanBakhFaNum-Light.woff2",
+      weight: "300",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/yekanbakh/YekanBakhFaNum-Regular.woff2",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/yekanbakh/YekanBakhFaNum-SemiBold.woff2",
+      weight: "600",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/yekanbakh/YekanBakhFaNum-Bold.woff2",
+      weight: "700",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/yekanbakh/YekanBakhFaNum-Black.woff2",
+      weight: "900",
+      style: "normal",
+    },
+  ],
+  variable: "--font-yekanbakh",
+});
 
 export default function RootLayout({
   children,
@@ -6,8 +53,17 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html
+      dir="rtl"
+      className={`dark ${figtree.variable} ${yekanbakh.variable}`}
+    >
+      <body className="min-h-screen grid grid-rows-[80px_1fr_auto] dark:bg-base-100 dark:text-base-content">
+        <Header />
+        <div className="flex flex-1 justify-center items-center">
+          {children}
+        </div>
+        <Footer />
+      </body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div>
-      <h1>React application architecture</h1>
+      {/* <h1>React application architecture</h1> */}
     </div>
   );
 }
