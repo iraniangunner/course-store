f5a7f477d21da612a2d6d77bda9059dcf2e9e8d1
iraniangunner <fateh19950@hotmail.com>
2024-11-27 00:31:14 +0330

    fetch api added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,23 @@
+import { CourseSummary } from "@/types/course-summary.interface";
 import { HomeHeroSection } from "./_components/home-hero-section/home-hero-section";
-export default function Home() {
+
+async function getNewestCourses(count: number): Promise<CourseSummary[]> {
+  const res = await fetch(
+    `https://api.classbon.com/api/courses/newest/${count}`,
+    {
+      next: { revalidate: 24 * 60 * 60 },
+    }
+  );
+  return res.json();
+}
+export default async function Home() {
+  const newestCourses = await getNewestCourses(4);
   return (
     <>
       <HomeHeroSection />
+      {newestCourses.map((p) => (
+        <p key={p.title}>{p.title}</p>
+      ))}
     </>
   );
 }
