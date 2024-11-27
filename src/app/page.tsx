4e4457519e10f53e168ae3920789a61e5e4c2ed1
iraniangunner <fateh19950@hotmail.com>
2024-11-28 00:47:11 +0330

    card component added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import { CourseSummary } from "@/types/course-summary.interface";
 import { HomeHeroSection } from "./_components/home-hero-section/home-hero-section";
+import { CourseCardList } from "./(courses)/_components/course-card-list";
 
 async function getNewestCourses(count: number): Promise<CourseSummary[]> {
   const res = await fetch(
@@ -15,9 +16,15 @@ export default async function Home() {
   return (
     <>
       <HomeHeroSection />
-      {newestCourses.map((p) => (
-        <p key={p.title}>{p.title}</p>
-      ))}
+      <section className="container  pt-20">
+        <div className="text-center xl:text-right">
+          <h2 className="text-2xl font-extrabold">تازه ترین دوره های آموزشی</h2>
+          <p className="mt-3 text-lg">
+            برای به‌روز موندن، یاد گرفتن نکته‌های تازه ضروری‌ه!
+          </p>
+        </div>
+        <CourseCardList courses={newestCourses} />
+      </section>
     </>
   );
 }
