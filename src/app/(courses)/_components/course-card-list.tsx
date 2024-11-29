8c713fb1b485b6b812bdc27c6be3703ce7b57247
iraniangunner <fateh19950@hotmail.com>
2024-11-29 22:45:59 +0330

    procced until features

--- src/app/(courses)/_components/course-card-list.tsx
+++ src/app/(courses)/_components/course-card-list.tsx
@@ -1,14 +1,14 @@
 import React from "react";
 import { CourseSummary } from "@/types/course-summary.interface";
-
 import { CourseCard } from "./course-card";
-
 
 type CourseCardListProps = {
   courses: CourseSummary[];
-}
+};
 
-export const CourseCardList: React.FC<CourseCardListProps> = async ({courses}) => {
+export const CourseCardList: React.FC<CourseCardListProps> = async ({
+  courses,
+}) => {
   return (
     <div className="flex flex-wrap justify-center xl:justify-start gap-6 mt-10">
       {courses.map((course) => (
