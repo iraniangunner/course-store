a0a598292c1e5e800b2625b63c7f348c157e0caf
iraniangunner <fateh19950@hotmail.com>
2024-12-05 18:45:28 +0330

    text-placeholder added for comment loading

--- src/app/(courses)/courses/[slug]/_components/comments/course-comments.tsx
+++ src/app/(courses)/courses/[slug]/_components/comments/course-comments.tsx
@@ -1,11 +1,13 @@
 "use client";
+
 import { useParams } from "next/navigation";
 import { useCourseComments } from "../../_api/get-comments";
 import { Comment } from "@/app/_components/comment";
+import { TextPlaceholder } from "@/app/_components/placeholders";
 
 const CourseComments = () => {
   const { slug } = useParams();
-  const { data: comments } = useCourseComments({
+  const { data: comments, isLoading } = useCourseComments({
     params: {
       slug: slug as string,
       page: 1,
@@ -16,6 +18,8 @@ const CourseComments = () => {
       {comments?.data.map((comment) => (
         <Comment key={`comment-${comment.id}`} {...comment} variant="info" />
       ))}
+
+      {isLoading && <TextPlaceholder />}
     </>
   );
 };
